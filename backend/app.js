93491b3c743c04b652211a502730646c11cfa87f
1660612 <1660612@student.hcmus.edu.vn>
2020-12-26 22:48:14 +0700

    api login

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var touristPackageRouter = require('./routes/touristPackage');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/users', usersRouter);
 app.use('/tourist_package', touristPackageRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/utils/globals.js
@@ -0,0 +1,5 @@
+const constant = {
+    secretKeys: 'Sm@rtEasyTravel'
+}
+
+module.exports = constant;
