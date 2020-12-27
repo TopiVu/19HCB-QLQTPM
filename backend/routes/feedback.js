7dbb545d655f3a072c044022047be69c1e94a906
1660612 <1660612@student.hcmus.edu.vn>
2020-12-27 12:26:00 +0700

    apidocs

--- backend/routes/feedback.js
+++ backend/routes/feedback.js
@@ -4,6 +4,25 @@ var validation = require('../middlewares/validate.mdw');
 var feedbackModel = require('../models/feedback.model');
 var constant = require('../utils/globals');
 
+/**
+ * @api {get} /feedback/:tourist_package_id Get feedback of tourism
+ * @apiName Get feedback of tourism
+ * @apiGroup Tourism
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ * {
+ *     "data": [
+ *         {
+ *             "feedback_id": 1,
+ *             "feedback": "Good",
+ *             "rate": 5,
+ *             "user_id": 1,
+ *             "tourist_package_id": 1
+ *         }
+ *     ]
+ * }
+ */
 router.get('/:tourist_package_id', validation(require('../schemas/searchCommon.json')), function (req, res, next) {
   var tourist_package_id = req.params.tourist_package_id;
   
--- backend/routes/touristPackage.js
+++ backend/routes/touristPackage.js
@@ -16,6 +16,37 @@ router.get('/:id/show', function (req, res, next) {
   res.render('index', { page: 'components/touristPackage/show' });
 });
 
+/**
+ * @api {get} /tourist_package/findByName Find tourism by name
+ * @apiName Find tourism by name
+ * @apiGroup Tourism
+ * 
+ * @apiParamExample {json} Request-Example:
+ * {
+ *     "search": "tour"
+ * }
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ * {
+ *     "data": [
+ *         {
+ *             "tourist_package_id": 1,
+ *             "name": "tour 1",
+ *             "content": "content",
+ *             "image_path": null,
+ *             "price": "150000.00",
+ *             "start_date": "2020-12-26T17:38:57.000Z",
+ *             "end_date": null,
+ *             "expired_date": null,
+ *             "status": 1,
+ *             "min_capacity": null,
+ *             "max_capacity": null,
+ *             "company_id": 1
+ *         }
+ *     ]
+ * }
+ */
 router.get('/findByName', validation(require('../schemas/searchCommon.json')), function (req, res, next) {
   var searchString = req.body.search || '';
   touristPackageModel.findByName(searchString).then(touristPackages => {
@@ -30,6 +61,30 @@ router.get('/findByName', validation(require('../schemas/searchCommon.json')), f
   })
 });
 
+/**
+ * @api {get} /tourist_package/details/:id Get details of tourism
+ * @apiName Get details of tourism
+ * @apiGroup Tourism
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ * {
+ *     "data": {
+ *         "tourist_package_id": 1,
+ *         "name": "tour 1",
+ *         "content": "content",
+ *         "image_path": null,
+ *         "price": "150000.00",
+ *         "start_date": "2020-12-26T17:38:57.000Z",
+ *         "end_date": null,
+ *         "expired_date": null,
+ *         "status": 1,
+ *         "min_capacity": null,
+ *         "max_capacity": null,
+ *         "company_id": 1
+ *     }
+ * }
+ */
 router.get('/details/:id', function (req, res) {
   const tour_id = req.params.id;
 
