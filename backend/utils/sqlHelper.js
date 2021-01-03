af4b73d26724044770f0c480a3b313f7df8c06a5
hienviluong125 <hienviluong125@gmail.com>
2021-01-03 20:51:41 +0700

    change search query from OR -> AND

--- backend/utils/sqlHelper.js
+++ backend/utils/sqlHelper.js
@@ -2,7 +2,7 @@ const buildSearchQuery = function (attributes, tableName) {
   let queryStr = `SELECT * FROM ${tableName}`;
 
   for (attrKey in attributes) {
-    const additionalSyntax = queryStr.includes("WHERE") ? "OR" : "WHERE"
+    const additionalSyntax = queryStr.includes("WHERE") ? "AND" : "WHERE"
     if (attrKey && attributes[attrKey]) {
       queryStr = queryStr + ` ${additionalSyntax} LOWER(${tableName}.${attrKey}) like '%${attributes[attrKey]}%'`;
     }
